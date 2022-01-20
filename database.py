08e927b5d7a6833d5c79da6d6ee1d314a64e98b3
yutanov <utanov@gmail.com>
2022-01-20 20:49:58 +0300

    fix vk_bot

---
+++ database.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import psycopg2
+from psycopg2 import Error
+import itertools
+
+try:
+    # connection settings
+    connection = psycopg2.connect(user="",
+                                  password="",
+                                  host="",
+                                  port="",
+                                  database="")
+
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT name FROM category")
+    categories = list(itertools.chain(*cursor.fetchall()))
+
+    cursor.execute("SELECT name FROM goods WHERE category_id='1'")
+    cat1 = list(itertools.chain(*cursor.fetchall()))
+
+    cursor.execute("SELECT name FROM goods WHERE category_id='2'")
+    cat2 = list(itertools.chain(*cursor.fetchall()))
+
+    cursor.execute("SELECT name FROM goods WHERE category_id='3'")
+    cat3 = list(itertools.chain(*cursor.fetchall()))
+
+    cursor.execute("SELECT description FROM goods")
+    descriptions = list(itertools.chain(*cursor.fetchall()))
+
+    cursor.execute("SELECT name FROM goods")
+    names = list(itertools.chain(*cursor.fetchall()))
+
+    cursor.execute("SELECT image FROM goods ORDER BY image")
+    images = list(itertools.chain(*cursor.fetchall()))
+
+except (Exception, Error) as error:
+    print("Ошибка при работе с PostgreSQL", error)
+finally:
+    if connection:
+        cursor.close()
+        connection.close()
---
+++ vk_bot.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+
+import itertools
+import database
+from vkbottle.bot import Bot, Message
+from vkbottle import Keyboard, KeyboardButtonColor, Text
+
+# connection to vk
+group_id = ''
+secret = ''
+
+bot_token = secret
+bot_group_id = group_id
+vk = Bot(bot_token, bot_group_id)
+
+
+# getting data from database
+cat1 = database.categories[0]
+cat2 = database.categories[1]
+cat3 = database.categories[2]
+
+cat = [cat1, cat2, cat3]
+
+goods_cat1 = database.cat1
+goods_cat2 = database.cat2
+goods_cat3 = database.cat3
+items = [
+goods_cat1,
+goods_cat2,
+goods_cat3,
+]
+
+list_of_goods = list(itertools.chain(*items))
+
+names = database.names
+desc = database.descriptions
+images = database.images
+items_and_desc = dict(zip(names, desc))
+
+
+# keyboard menu
+@vk.on.message(text=[
+    'Начать', 'начать', '/mm', 'menu', 'меню', 'Menu', 'Меню', 'Start', 'start',
+    'Привет', 'привет', 'Ку', 'ку',
+])
+@vk.on.message(payload={'cmd': 'menu'})
+async def menu(message: Message):
+    await message.answer(
+        message='Что бы вы пожелали?',
+        keyboard=(
+            Keyboard(one_time=False, inline=False)
+            .add(Text(cat1), color=KeyboardButtonColor.POSITIVE)
+            .add(Text(cat2), color=KeyboardButtonColor.POSITIVE)
+            .row()
+            .add(Text(cat3), color=KeyboardButtonColor.POSITIVE)
+        )
+    )
+
+
+@vk.on.message(text = cat)
+async def menu(message: Message):
+    msg = message.text
+    i = cat.index(msg)
+    await message.answer(
+        message=msg,
+        keyboard=(
+            Keyboard(one_time=False, inline=False)
+            .add(Text(items[i][0]), color=KeyboardButtonColor.POSITIVE)
+            .add(Text(items[i][1]), color=KeyboardButtonColor.POSITIVE)
+            .row()
+            .add(Text(items[i][2]), color=KeyboardButtonColor.POSITIVE)
+            .add(Text(items[i][3]), color=KeyboardButtonColor.POSITIVE)
+            .row()
+            .add(Text('Назад', payload={'cmd': 'menu'}))
+        )
+    )
+
+
+@vk.on.message(text = list_of_goods)
+async def menu(message: Message):
+    msg = message.text
+    i = list_of_goods.index(msg)
+    await message.answer(
+        attachment=images[i],
+        message=items_and_desc[msg],
+        keyboard=(
+            Keyboard(one_time=False, inline=False)
+            .add(Text('Назад', payload={'cmd': 'menu'}))
+        )
+    )
+
+
+@vk.on.message()
+async def main(message):
+    await message.answer('Пожалуйста, выберите что-нибудь из этих вкусностей :)')
+
+
+vk.run_forever()
